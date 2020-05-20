b6dad1d7cd7a75bb43bdf989379255ae283ac344
Bình <binh.hathanh@vietis.com.vn>
2020-05-20 20:00:53 +0700

    Update jwt & passport

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,27 @@
 import { ConflictException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
-import { LoginDTO, RegisterDTO } from '../models/user.dto';
+import { LoginDTO, RegisterDTO } from '../models/user.model';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '../entities/user.entity';
 import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserEntity)
-    private userRepository: Repository<UserEntity>
+    private userRepository: Repository<UserEntity>,
+    private jwtService: JwtService
   ) {}
 
   async register(credentials: RegisterDTO) {
     try {
       const user = this.userRepository.create(credentials);
       await user.save();
+      const payload = { username: user.username };
+      // const token = this.jwtService.sign(payload);
+      const token = '123456789';
+
+      return { user: { ...user.toJSON(), token } };
     } catch (error) {
       /* Check username duplicate, error code 23505 */
       if (error.code === '23505') {
@@ -31,7 +38,11 @@ export class AuthService {
       if (!isValid) {
         throw new UnauthorizedException('Invalid credentials');
       }
-      return user;
+      const payload = { username: user.username };
+      // const token = this.jwtService.sign(payload);
+      const token = '123456789';
+
+      return { user: { ...user.toJSON(), token } };
     } catch (error) {
       throw new UnauthorizedException('Invalid credentials');
     }
